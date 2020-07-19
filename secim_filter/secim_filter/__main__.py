1c230ff0278f454d4b813da07f8254cf86732aa7
bebaek <bebaek@gmail.com>
2020-07-19 01:24:36 +0100

    Parse args in main()

--- secim_filter/secim_filter/__main__.py
+++ secim_filter/secim_filter/__main__.py
@@ -1,5 +1,19 @@
+import argparse
+
+from .secim_filter import secim_filter
+
+
 def main():
-    pass
+    """Entry point."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'input_path', help='input image root directory',
+        'output_path', help='output image root directory',
+        'last_minutes', help='last minutes to look',
+    )
+    args = parser.parse_args()
+
+    secim_filter(args.input_path, args.output_path, args.last_minutes)
 
 
 if __name__ == '__main__':
